42c8ac706b57c86aa76ec8af14e90ccf91b22363
leexsoft-mac <leexsoft@163.com>
2019-04-12 14:26:32 +0800

    初始提交

---
+++ Api1/Controllers/TimeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TimeController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public JsonResult Get()
+        {
+            var now = DateTime.Now;
+            return Json(now);
+        }
+    }
+}
---
+++ Api2/Controllers/FeeController.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeeController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public JsonResult Get()
+        {
+            return Json (new  {  amount=10, onlinePay=8, unPay=2 });
+        }
+    }
+}
